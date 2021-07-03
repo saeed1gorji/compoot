e8b87ff76fccdbb3edc0bd24b46e733c2ced49f5
Saeed Gorji <saeed1gorji@gmail.com>
2021-07-04 00:29:16 +0430

    Add docker-compose down command with down argument.
    Down bring the whole compose project down and deletes
    containers. It is a way of refresh things and may be
    useful in some situations.

--- stop-proxy.sh
+++ stop-proxy.sh
@@ -7,23 +7,30 @@ pushd $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd ) >/dev/nu
 proxy_service=ovpn
 
 socks_port=2221
+ssh_port=2223
 pid=$(ps --no-heading --format pid,cmd | grep -E "ssh.*$socks_port" | grep -v grep | awk '{print $1}')
 if [ ! -z "$pid" ]; then
     kill -15 $pid
     echo "SSH tunnel (pid=$pid) terminated."
 fi
 
-set +e
-sudo docker exec $proxy_service pkill -15 openvpn 2>/dev/null
-sudo docker exec $proxy_service service sshd stop 2>/dev/null
-[ $? -eq 1 ] && echo "$proxy_service is already down." && [ ! "$1" = "restart" ] && exit 0
-set -e
+function stop_service()
+{
+    set +e
+    sudo docker exec $proxy_service pkill -15 openvpn 2>/dev/null
+    sudo docker exec $proxy_service service sshd stop 2>/dev/null
+    [ $? -eq 1 ] && echo "$proxy_service is already down." && [ ! "$1" = "restart" ] && exit 0
+    set -e
+}
 
-if [ "kill" = "$1" ]; then
-    sudo docker-compose kill $proxy_service
+if [ "all" = "$1" -o "down" = "$1" ]; then
+    sudo docker-compose down --timeout=2
+    ssh-keygen -f "$HOME/.ssh/known_hosts" -R "[localhost]:$ssh_port"
 elif [ "restart" = "$1" ]; then
+    stop_service
     sudo docker-compose restart --timeout=2 $proxy_service
 else
+    stop_service
     sudo docker-compose stop --timeout=2 $proxy_service
 fi
 
